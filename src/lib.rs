1fbc87170136753e6765519b3ff5fc9010e02e44
Dumpinground <e60533658@icloud.com>
2023-01-24 13:20:56 +0800

    u

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,15 @@
-fn a_b_c() {
+pub fn a_b_c() {
     let abc = "A_B_C";
     println!("say {}", abc);
+}
+
+#[cfg(test)]
+mod tests {
+
+    use crate::a_b_c;
+
+    #[test]
+    fn test1() {
+        a_b_c();
+    }
 }
